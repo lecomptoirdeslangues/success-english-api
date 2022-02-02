0ca63502f105e01816c022919bf9719d85f064cd
cmrweb <tykrastagames@gmail.com>
2022-02-02 10:36:14 +0100

    add get user

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
-//initialisation
+//Initialisation
 require "vendor/autoload.php";
+//Lecture du .env
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+//Utilisation
 use SuccessEnglish\Api;
 
 $api = new Api($_ENV["SE_PUBLIC_KEY"],$_ENV["SE_PRIVATE_KEY"]);
 
-echo $api->getUsers("json");
-var_dump($api->getUsers());
+echo $api->getUsers()->getData("json");
+// var_dump($api->getUsers()->getData());
+
+var_dump($api->getUser(0)->getData("json"));
--- src/Api.php
+++ src/Api.php
@@ -5,19 +5,34 @@ namespace SuccessEnglish;
 class Api
 {
     private $publicKey, $privateKey;
-    private $data;
-
+    private $data; 
+ 
     public function __construct(string $publicKey, string $privateKey)
     {
         $this->publicKey = $publicKey;
         $this->privateKey = $privateKey;
         return $this;
     }
-
-    public function getUsers(string $format = null)
+    private function connect(string $param)
     {
         //success-english.fr
-        $this->data = file_get_contents("http://localhost:8070/api/{$this->publicKey}/{$this->privateKey}/users");
+        return file_get_contents("http://localhost:8070/api/{$this->publicKey}/{$this->privateKey}/{$param}");
+    }
+
+    public function getUsers()
+    {
+        $this->data = $this->connect("users");
+        return $this;
+    }
+
+    public function getUser(int $id)
+    {
+        $this->data = $this->connect("user/{$id}"); 
+        return $this;
+    }
+
+    public function getData(string $format = null)
+    {
         return $format == "json" ? $this->data : json_decode($this->data, true);
     }
 }
